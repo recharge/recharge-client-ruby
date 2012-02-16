85a19c0552e1d4d6e1d5dd1137a0b2d7f9ff8f26
Eric Cardin <eric.cardin@rechargebilling.com>
2012-02-16 00:56:56 -0800

    first commit

--- lib/recharge.rb
+++ lib/recharge.rb
@@ -16,44 +16,4 @@ module Recharge
     end
     attr_writer :api_key
   end
-  
-  def get(uri)
-    client = Client.new()
-    return client.request('GET', uri)
-  end
-  
-  def post(uri, data=nil)
-    client = Client.new()
-    return client.request('POST', uri, data)
-  end
-  
-  def delete(uri)
-    client = Client.new()
-    return client.request('DELETE', uri)
-  end
-  
-  def returnImportantXML(data)
-    require "rexml/document"
-    doc = REXML::Document.new(data)
-    
-    if doc.elements[1].elements[3].length == 1
-      return doc.elements[1].elements[3].elements[1]
-    else 
-      return doc.elements[1].elements[3]
-    end
-  end
-  
-  def returnErrorXML(data)
-    require "rexml/document"
-    doc = REXML::Document.new(data)
-    
-    doc.elements["/response/result/resultDescription"].text
-  end
-  
-  def returnStatusXML(data)
-    require "rexml/document"
-    doc = REXML::Document.new(data)
-    
-    doc.elements["/response/result/resultDescription"]
-  end 
 end
